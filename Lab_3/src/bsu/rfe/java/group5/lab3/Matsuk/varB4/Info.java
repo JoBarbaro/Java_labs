7a7ef09c1281aa8b628c25838a2deb28fbef8c3e
JoBarbaro <150555133+JoBarbaro@users.noreply.github.com>
2023-12-16 23:02:19 +0300

    Delete Lab_3 directory

--- Lab_3/src/bsu/rfe/java/group5/lab3/Matsuk/varB4/Info.java
+++
@@ -1,9 +0,0 @@
-package bsu.rfe.java.group5.lab3.Matsuk.varB4;
-
-public class Info {
-    private final String Surname=" Skaletta";
-    private final int group=5;
-    public String to_string() {
-    return "Cтудент:" + Surname + " "+ "Vito"+ " " + group;
-    }
-}
